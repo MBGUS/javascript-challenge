11d005898fdc98bb4bf70afb84b45e6983c424c8
Gustavo M <gusmaldonadob@gmail.com>
2021-06-26 17:31:15 -0500

    Create the table

--- UFO-Level_1/static/js/app.js
+++ UFO-Level_1/static/js/app.js
@@ -1,4 +1,27 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select the table body 
+var tbody = d3.select("tbody")
+
+// Loop through data and append rows to the table body.
+// Use d3 to update each cell's text with UFO report values (Date, City, State, Country, Shape, Duration, Comment).
+
+tableData.forEach(function(ufo){            // Two ways: declaring a function or using arrow =>.
+    var row = tbody.append("tr");           // example: data.forEach((ufo) => {}
+    
+    // use append method to insert table data for each row
+    
+    Object.entries(ufo).forEach(function([key,value]){
+        // console.log(key, value);         // Verify the print of each key&value for UFO report.
+        
+        // Function to create the table:
+        // Use append to insert a cell for each value in the UFO Report.
+        // Use text to insert data to each cell.
+
+        var cell = row.append("td").text(value);    
+    });
+});
+
+// Select the button and create function
+
